5bc64b552175025161f2c25d1e4613f86642b9ad
shivamswarnkar <ss8464@nyu.edu>
2021-05-15 07:31:03 +0530

    refactor vgg

--- models/vgg/model.py
+++ models/vgg/model.py
@@ -3,12 +3,12 @@
 Returns:
     Module: Torch Module of VGG16.
 """
-from torch.nn import Module
-from torchsummary import summary
 import torchvision.models as models
 
+from models.base_model import Model
 
-class VGG16:
+
+class VGG16(Model):
     """Loads VGG16 model.
     """
     def __init__(self, batch_norm=False) -> None:
@@ -17,27 +17,13 @@ class VGG16:
         Args:
             batch_norm (bool, optional): Include batch normalization layer. Defaults to False.
         """
-        self._model = models.vgg16_bn(
+        # load model
+        vgg16_model = models.vgg16_bn(
             pretrained=True) if batch_norm else models.vgg16(pretrained=True)
+
+        # setup
+        super().__init__(model=vgg16_model)
         self._bn = batch_norm
-
-        # Set model to eval mode
-        self._model.cuda()
-        self._model.eval()
-
-    @property
-    def model(self) -> Module:
-        """Getter for the model
-
-        Returns:
-            Module: torch model
-        """
-        return self._model
-
-    def print_summary(self) -> None:
-        """Print summary of the model.
-        """
-        print(summary(self._model, input_size=(3, 224, 224)))
 
 
 if __name__ == "__main__":
