29b9b6fe1f9f4643b9e4f47c7b0f0927e4f0e18e
makaveli <39617050+makaveli10@users.noreply.github.com>
2021-05-16 18:07:55 +0530

    Revert "Refactor and Add Tests (#4)"
    This reverts commit 2f71ca6ef824defb6a28402ede56e8a0f0303bda.

--- models/inception_v4/model.py
+++ models/inception_v4/model.py
@@ -2,74 +2,74 @@ import json
 import torch.utils.model_zoo as model_zoo
 from torch import nn
 
-from models.base_model import Model
-
-from . import inception
+import inception
 
 
-class InceptionV4(Model):
+class InceptionV4:
     """Loads Inceptionv4 model.
     """
-    def __init__(self,
-                 num_classes=1000,
-                 config='config.json',
-                 pretrained='imagenet') -> None:
+    def __init__(self, num_classes=1000, config='config.json', pretrained='imagenet') -> None:
         """Initialize inception v4 model
         """
+        self._model = None
+        
         # load config
-        with open(config, 'r') as f:
-            config = json.load(f)
+        with open(config) as f:
+            self._config = json.load(f)
 
-        inception_model = self.load_model(num_classes, pretrained, config)
+        self._load_model(num_classes, pretrained)
+        
+        # Set model to eval mode
+        self._model.cuda()
+        self._model.eval()
 
-        super().__init__(model=inception_model, input_size=(3, 299, 299))
+    @property
+    def model(self) -> nn.Module:
+        """Getter for the model
 
-    @staticmethod
-    def load_model(num_classes: int, pretrained: str,
-                   config: dict) -> inception.Inception_v4:
+        Returns:
+            nn.Module: torch model
+        """
+        return self._model
+
+    def print_summary(self) -> None:
+        """Print summary of the model.
+        """
+        print(summary(self._model, input_size=(3, 299, 299)))
+
+    def _load_model(self, num_classes: int, pretrained: str) -> None:
         """Creates the inceptionv4 model and loads state dict.
 
         Args:
             num_classes (int): number of classes in the classifier
-            pretrained (str): If True, returns a model pre-trained on ImageNet.
-            config (dict): Configurations for setting up model.
-        Raises:
-            ValueError: raised if num classes don't match with the settings.
-
-        Returns:
-            inception.Inception_v4: loaded Inception model
+            pretrained (bool): If True, returns a model pre-trained on ImageNet. 
         """
-
-        model = None
         if pretrained:
-            settings = config['inceptionv4'][pretrained]
+            settings = self._config['inceptionv4'][pretrained]
 
             if num_classes != settings['num_classes']:
-                raise ValueError(
-                    f'num_classes should be {settings["num_classes"]}, but is {num_classes}'
-                )
+                raise ValueError(f'num_classes should be {settings["num_classes"]}, but is {num_classes}')
 
             # both 'imagenet'&'imagenet+background' are loaded from same parameters
-            model = inception.Inception_v4(num_classes=1001)
-            model.load_state_dict(model_zoo.load_url(settings['url']))
+            self._model = inception.Inception_v4(num_classes=1001)
+            self._model.load_state_dict(model_zoo.load_url(settings['url']))
 
             if pretrained == 'imagenet':
                 new_last_linear = nn.Linear(1536, 1000)
-                new_last_linear.weight.data = model.last_linear.weight.data[1:]
-                new_last_linear.bias.data = model.last_linear.bias.data[1:]
-                model.last_linear = new_last_linear
+                new_last_linear.weight.data = self._model.last_linear.weight.data[1:]
+                new_last_linear.bias.data = self._model.last_linear.bias.data[1:]
+                self._model.last_linear = new_last_linear
 
-            model.input_space = settings['input_space']
-            model.input_size = settings['input_size']
-            model.input_range = settings['input_range']
-            model.mean = settings['mean']
-            model.std = settings['std']
+            self._model.input_space = settings['input_space']
+            self._model.input_size = settings['input_size']
+            self._model.input_range = settings['input_range']
+            self._model.mean = settings['mean']
+            self._model.std = settings['std']
         else:
-            model = inception.Inception_v4(num_classes=num_classes)
-
-        return model
+            self._model = inception.Inception_v4(num_classes=num_classes)
 
 
 if __name__ == '__main__':
     net = InceptionV4(num_classes=1000, pretrained='imagenet')
     print(net.model)
+    
--- models/resnet/wide_resnet.py
+++ models/resnet/wide_resnet.py
@@ -3,18 +3,36 @@
 Returns:
     Module: Torch Module of WideResnet.
 """
-from torchvision.models import wide_resnet50_2
+from torch.nn import Module
+from torchsummary import summary
+import torchvision.models as models
 
-from models.base_model import Model
 
-
-class WideResnet(Model):
+class WideResnet:
     """Loads WideResnet121 model.
     """
     def __init__(self) -> None:
         """Initialize WideResnet121 model.
         """
-        super().__init__(model=wide_resnet50_2(pretrained=True))
+        self._model = models.wide_resnet50_2(pretrained=True)
+
+        # Set model to eval mode
+        self._model.cuda()
+        self._model.eval()
+
+    @property
+    def model(self) -> Module:
+        """Getter for the model
+
+        Returns:
+            Module: torch model
+        """
+        return self._model
+
+    def print_summary(self) -> None:
+        """Print summary of the model.
+        """
+        print(summary(self._model, input_size=(3, 224, 224)))
 
 
 if __name__ == "__main__":
