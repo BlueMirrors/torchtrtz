2f71ca6ef824defb6a28402ede56e8a0f0303bda
Shivam Swanrkar <ss8464@nyu.edu>
2021-05-16 17:49:11 +0530

    Refactor and Add Tests (#4)
    * add base model class
    
    * refactor alexnet
    
    * use pretrained weights
    
    * refactor densenet
    
    * refactor wide_resnet
    
    * refactor vgg
    
    * refactor inception
    
    * refactor generate_weights
    
    * set default model
    
    * add tests
    
    * add test info to readme
    
    * fix inception errors

--- models/alexnet/model.py
+++ models/alexnet/model.py
@@ -3,36 +3,19 @@
 Returns:
     Module: Torch Module of AlexNet.
 """
-from torch.nn import Module
-from torchsummary import summary
 from torchvision.models import alexnet
 
+from models.base_model import Model
 
-class AlexNet:
+
+class AlexNet(Model):
     """Loads AlexNet model.
     """
     def __init__(self) -> None:
         """Initialize AlexNet model.
         """
-        self._model = alexnet(pretrained=True)
-
-        # Set model to eval mode
-        self._model.cuda()
-        self._model.eval()
-
-    @property
-    def model(self) -> Module:
-        """Getter for the model
-
-        Returns:
-            Module: torch model
-        """
-        return self._model
-
-    def print_summary(self) -> None:
-        """Print summary of the model.
-        """
-        print(summary(self._model, input_size=(3, 224, 224)))
+        # setup
+        super().__init__(model=alexnet(pretrained=True))
 
 
 if __name__ == "__main__":
